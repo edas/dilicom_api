4a1e3d62c0bbb140717bc115907139354305015e
Eric Daspet <eric.daspet@survol.fr>
2013-11-13 15:41:50 +0100

    allow named arguments in #get_notices

--- lib/dilicom_api/hub/api/get_notices.rb
+++ lib/dilicom_api/hub/api/get_notices.rb
@@ -4,19 +4,19 @@ require 'active_support/time'
 module DilicomApi
   module Hub
     class Client
-      def get_notices(options)
+      def get_notices(param=nil, since: nil)
         end_point = '/v1/hub-numerique-api/json/getNotices'
         params = { }
-        case options
+        case param
         when :initialization
           params["initialization"] = nil
         when :last_connection
           params["lastConnection"] = nil
         else
-          if options.has_key? :since
-            since = options[:since].in_time_zone(DILICOM_TIMEZONE)
-            params["sinceDate"]  = since.iso8601.gsub(/\+.*/,'')
-          end
+          since ||= param
+          raise "at least a parameter is required in #get_notices" if since.nil?
+          since = since.in_time_zone(DILICOM_TIMEZONE)
+          params["sinceDate"]  = since.iso8601.gsub(/\+.*/,'')
         end
         data = json_request(end_point, params, timeout: 180)
         data.has_key?('noNotice') ? [ ] : data['onixFileUrls'].map { |e| e['httpLink'] }

cb3f02a944aa264faf2233e6d81cb07c6bcb491e
Eric Daspet <eric.daspet@survol.fr>
2013-11-22 22:01:28 +0100

    more tests for #get_notices_for_distributor

--- spec/hub/api/get_notices_spec.rb
+++ spec/hub/api/get_notices_spec.rb
@@ -9,7 +9,6 @@ describe DilicomApi::Hub::Client do
   # Generic get_notices tests 
   shared_examples "get_notices" do
     include_context "faraday connection" 
-    let(:end_point) { "/v1/hub-numerique-api/json/getNotices" }
     let(:onix_urls) {
       [
         "https://hub-dilicom.centprod.com/notices_onyx/diffusion_3025594195700_201008272128_1110247456419854240546744705832927698715.xml", 
@@ -93,7 +92,9 @@ describe DilicomApi::Hub::Client do
           end
         end
         date = DateTime.now
-        subject.send(method, since: date)
+        params = method_parameters
+        params << { since: date }
+        subject.send(method, *params)
         expect(options).to have_key("sinceDate")
       end
       it "should use french timezone" do
@@ -105,7 +106,9 @@ describe DilicomApi::Hub::Client do
           end
         end
         date = DateTime.now
-        subject.send(method, since: date)
+        params = method_parameters
+        params << { since: date }
+        subject.send(method, *params)
         received = options["sinceDate"]
         zone = ActiveSupport::TimeZone.new("Europe/Paris")
         expect(date.to_i).to eq(zone.parse(received).to_i)
@@ -120,56 +123,144 @@ describe DilicomApi::Hub::Client do
             [200, {}, message]
           end
         end
-        subject.send(method, :last_connection)
+        params = method_parameters
+        params << :last_connection
+        subject.send(method, *params)
         expect(options).to have_key("lastConnection")
       end
+    end
+  end
+
+  shared_examples "with_distributor" do
+    it "should send the distributor" do
+      options = { }
+      set_connection do |stub|
+        stub.get(end_point) do |env|  
+          options = env[:params]
+          [200, {}, message]
+        end
+      end
+      subject.send(method, *method_parameters)
+      expect(options).to have_key("glnDistributor")
+      expect(options["glnDistributor"]).to eq(gln_distributor)
     end
   end
 
   # Actual tests
-  
-  describe "#get_notices" do
-    let(:method) { :get_notices }
-    include_examples "get_notices" do
-      let(:method_parameters) { [ since: DateTime.now ]  }
-    end
-    context "when called with :initialization" do
-      include_examples "all_notices (initialization)" do
-        let(:method_parameters) { [ :initialization ] }
+  context "when no distributor targeted" do
+    let(:end_point) { "/v1/hub-numerique-api/json/getNotices" }
+    
+    describe "#get_notices" do
+      let(:method) { :get_notices }
+      include_examples "get_notices" do
+        let(:method_parameters) { [ since: DateTime.now ]  }
+      end
+      context "when called with :initialization" do
+        include_examples "all_notices (initialization)" do
+          let(:method_parameters) { [ :initialization ] }
+        end
+      end
+      context "latest_notices" do
+        include_examples "latest_notices" do
+          let(:method_parameters) { [ ] }
+        end
       end
     end
-    include_examples "latest_notices"
-  end
 
-  describe "#all_notices" do
-    let(:method) { :all_notices }
-    include_examples "get_notices" do
-      let(:method_parameters) { [ ] }
+    describe "#all_notices" do
+      let(:method) { :all_notices }
+      include_examples "get_notices" do
+        let(:method_parameters) { [ ] }
+      end
+      include_examples "all_notices (initialization)" do
+        let(:method_parameters) { [ ] }
+      end
     end
-    include_examples "all_notices (initialization)" do
-      let(:method_parameters) { [ ] }
-    end
-  end
 
-  describe "#latest_notices" do
-    let(:method) { :latest_notices }
-    include_examples "get_notices" do
-      let(:method_parameters) { [ since: DateTime.now ]  }
+    describe "#latest_notices" do
+      let(:method) { :latest_notices }
+      include_examples "get_notices" do
+        let(:method_parameters) { [ since: DateTime.now ]  }
+      end
+      include_examples "latest_notices" do
+        let(:method_parameters) { [ ]  }
+      end
+      context "when called with no parameters" do
+        it "should call lastConnection" do
+          options = { }
+          set_connection do |stub|
+            stub.get(end_point) do |env|  
+              options = env[:params]
+              [200, {}, message]
+            end
+          end
+          subject.send(method)
+          expect(options).to have_key("lastConnection")
+        end
+      end
+
     end
-    include_examples "latest_notices"
-    context "when called with no parameters" do
-      it "should call lastConnection" do
-        options = { }
-        set_connection do |stub|
-          stub.get(end_point) do |env|  
-            options = env[:params]
-            [200, {}, message]
+
+    context "when a distributor is targeted" do
+      let(:end_point) { "/v1/hub-numerique-api/json/getNoticesForDistributor" }
+      let(:gln_distributor) { "321" }
+
+      describe "#get_notices_for_distributor" do
+        let(:method) { :get_notices_for_distributor }  
+        include_examples "get_notices" do
+          let(:method_parameters) { [ gln_distributor, since: DateTime.now ]  }
+        end
+        context "when called with :initialization" do
+          include_examples "all_notices (initialization)" do
+            let(:method_parameters) { [ gln_distributor, :initialization ] }
           end
         end
-        subject.send(method)
-        expect(options).to have_key("lastConnection")
+        context "latest_notices" do
+          include_examples "latest_notices" do
+            let(:method_parameters) { [ gln_distributor ] }
+          end
+        end
+        include_examples "with_distributor" do
+          let(:method_parameters) { [ gln_distributor, since: DateTime.now ] }
+        end
       end
+
+      describe "#all_notices_for_distributor" do
+        let(:method) { :all_notices_for_distributor }
+        let(:method_parameters) { [ gln_distributor ] }
+        include_examples "get_notices"
+        include_examples "all_notices (initialization)"
+        include_examples "with_distributor"
+      end
+
+      describe "#latest_notices_for_distributor" do
+        let(:method) { :latest_notices_for_distributor }
+        include_examples "get_notices" do
+          let(:method_parameters) { [ gln_distributor, since: DateTime.now ]  }
+        end
+        include_examples "with_distributor" do
+          let(:method_parameters) { [ gln_distributor, since: DateTime.now ]  }
+        end
+        include_examples "latest_notices" do
+          let(:method_parameters) { [ gln_distributor ]  }
+        end
+        context "when called with no parameters" do
+          it "should call lastConnection" do
+            options = { }
+            set_connection do |stub|
+              stub.get(end_point) do |env|  
+                options = env[:params]
+                [200, {}, message]
+              end
+            end
+            subject.send(method, gln_distributor)
+            expect(options).to have_key("lastConnection")
+          end
+        end
+      end
+
     end
+
   end
 end
 

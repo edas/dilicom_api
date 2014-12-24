bf581da628d9c7aa03c8e30d88529718b632a98a
Julien Fusco <jfusco@tea-ebook.com>
2014-12-24 09:38:32 +0100

    Rem: extra blank line and space

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,10 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'rspec'
-require 'dilicom_api' 
+require 'dilicom_api'
 require 'faraday_simulation'
 require 'active_support/all'
 require 'json'
-
-
 
 Dir[File.join(File.dirname(__FILE__),"support/**/*.rb")].each { |f| require f }
 

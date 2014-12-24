0258ff3cb3010f5ec95f3568432cb53bf14cbac1
Julien Fusco <jfusco@tea-ebook.com>
2014-12-24 09:39:23 +0100

    Fix: undefined method `color_enabled='
    with rspec 3.1.0

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,5 @@ Dir[File.join(File.dirname(__FILE__),"support/**/*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
   # some (optional) config here
-  config.color_enabled = true
   config.formatter     = 'documentation'
 end
